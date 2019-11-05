eec79a5abcd9b78bba51af41112e2c1da476bbf5
Fabian Saacke <fabiansaacke@googlemail.com>
2019-11-05 20:02:07 +0100

    some icons for sidebar drawer activity strings for multi language

---
+++ app/src/main/java/de/mobicom/notebookplusplus/shared/Sidebar.java
@@ -0,0 +1,15 @@
+package de.mobicom.notebookplusplus.shared;
+
+import androidx.appcompat.app.AppCompatActivity;
+import de.mobicom.notebookplusplus.R;
+
+import android.os.Bundle;
+
+public class Sidebar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sidebar);
+    }
+}
