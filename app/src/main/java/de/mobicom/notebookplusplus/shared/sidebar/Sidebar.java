33adf891de10c36fb7cb30800ad0de086282c73c
Fabian Saacke <fabiansaacke@googlemail.com>
2019-11-05 21:02:47 +0100

    changes

---
+++ app/src/main/java/de/mobicom/notebookplusplus/shared/sidebar/Sidebar.java
@@ -0,0 +1,10 @@
+package de.mobicom.notebookplusplus.shared.sidebar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import de.mobicom.notebookplusplus.R;
+
+import android.os.Bundle;
+
+public class Sidebar extends AppCompatActivity {
+
+}
