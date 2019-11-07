2bfd5c94e2895e30e33860898a52f5c9cae9f0fc
Fabian Saacke <fabiansaacke@googlemail.com>
2019-11-07 20:48:19 +0100

    changes

---
+++ app/src/main/java/de/mobicom/notebookplusplus/NotebooksFragment.java
@@ -0,0 +1,19 @@
+package de.mobicom.notebookplusplus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NotebooksFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notebooks, container, false);
+    }
+}
