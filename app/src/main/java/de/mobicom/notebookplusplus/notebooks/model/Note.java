a60c84451f47f31d5505a5baa155decc4f64aa5e
Fabian Saacke <fabiansaacke@googlemail.com>
2019-11-10 17:27:30 +0100

    models

--- app/src/main/java/de/mobicom/notebookplusplus/notebooks/model/Note.java
+++ app/src/main/java/de/mobicom/notebookplusplus/notebooks/model/Note.java
@@ -1,4 +1,79 @@
 package de.mobicom.notebookplusplus.notebooks.model;
 
+import java.util.Objects;
+
 class Note {
+
+    private int id;
+    private String name;
+    private String type;
+    private String description;
+
+    public Note() {
+    }
+
+    public Note(int id, String name, String type, String description) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Note)) return false;
+        Note note = (Note) o;
+        return getId() == note.getId() &&
+                getName().equals(note.getName()) &&
+                getType().equals(note.getType()) &&
+                getDescription().equals(note.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getType(), getDescription());
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
