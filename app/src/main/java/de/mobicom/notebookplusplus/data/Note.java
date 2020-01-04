c52c7ba3cba6eb62b770a1f075d066b2af037f98
Fabian Saacke <fabiansaacke@googlemail.com>
2020-01-04 15:07:00 +0100

    setup navigation

--- app/src/main/java/de/mobicom/notebookplusplus/data/Note.java
+++ app/src/main/java/de/mobicom/notebookplusplus/data/Note.java
@@ -9,13 +9,16 @@ import androidx.room.ForeignKey;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
+import static androidx.room.ForeignKey.CASCADE;
 import static androidx.room.ForeignKey.SET_NULL;
 
 @Entity(tableName = "note_table",
         foreignKeys = {
                 @ForeignKey(entity = Notebook.class,
                         parentColumns = "notebook_id",
-                        childColumns = "notebook_parent_id", onDelete = SET_NULL)},
+                        childColumns = "notebook_parent_id",
+                        onUpdate = CASCADE,
+                        onDelete = SET_NULL)},
         indices = {
                 @Index("notebook_parent_id")}
 )
--- app/src/main/java/de/mobicom/notebookplusplus/data/NotebookDatabase.java
+++ app/src/main/java/de/mobicom/notebookplusplus/data/NotebookDatabase.java
@@ -11,7 +11,7 @@ import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import de.mobicom.notebookplusplus.utils.LocalDateTimeConverter;
 
-@Database(entities = {Notebook.class, Note.class}, version = 3, exportSchema = false)
+@Database(entities = {Notebook.class, Note.class}, version = 5, exportSchema = false)
 @TypeConverters(LocalDateTimeConverter.class)
 public abstract class NotebookDatabase extends RoomDatabase {
 
